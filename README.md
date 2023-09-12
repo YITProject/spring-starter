0b9998a909ad49f0dc350cdaba55c0eb17a094d1
STARTRACEX <startraceebr@foxmail.com>
2023-09-12 12:18:12 +0800

    Add guidance

---
+++ README.md
@@ -0,0 +1,11 @@
+# spring-starter
+
+Spring project with kotlin
+
+restful api
+
+[/rest](http://localhost:8000/rest?name=xxx)
+
+template page
+
+[/web](http://localhost:8000/web)
--- src/main/kotlin/com/example/kotlin/controllers/rest.kt
+++ src/main/kotlin/com/example/kotlin/controllers/rest.kt
@@ -5,18 +5,6 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
-//@RestController
-//@Controller
-//class RestController {
-//    private val template = "Name:%s,Count:\"%d\""
-//    private val counter = AtomicLong()
-//
-//    @GetMapping("/rest")
-//    fun rest(@RequestParam(value = "name", defaultValue = "World") name: String?): String {
-//        counter.incrementAndGet()
-//        return String.format(template, name, counter.get())
-//    }
-//}
 @RestController
 class RESTController {
 
